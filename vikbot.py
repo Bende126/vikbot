65fc56cbe82e13099789ea6204d3d668b4fb1f96
Bende126 <bendi2001@gmail.com>
2020-08-22 13:22:46 +0200

    Add files via upload

--- vikbot.py
+++ vikbot.py
@@ -3,7 +3,14 @@ from discord.ext import commands, tasks
 import time, os
 from itertools import cycle
 
-client = commands.Bot(command_prefix = '!', status = discord.Status.idle, activity=discord.Game(name="Booting.."))
+prefixes = ['.', ',', '!!', 'xd']
+
+async def get_prefix(client, message):
+    if not message.guild:
+        return
+    return commands.when_mentioned_or(*prefixes)(client, message)
+
+client = commands.Bot(command_prefix = get_prefix, status = discord.Status.idle, activity=discord.Game(name="Booting.."))
 status = cycle(['Rossz csatorna', 'E M B E R', 'Literálisan cringe'])
  #botspam, admin botspam
 channelids =[739567794533826616, 745624827368570930]
@@ -13,7 +20,6 @@ async def on_ready():
 		print('Lets rock!')
 		print(client.user.name)
 		print(client.user.id)
-		print(client.guilds)
 		print("----------")
 		await client.change_presence(status=discord.Status.online, activity=discord.Game(name="with fire"))
 
@@ -26,10 +32,36 @@ async def on_message(message):
     if message.author == client.user:
         return
     else:
-        if message.channel.id in channelids:
+        if message.channel.id in channelids or message.author.id == 297689894208274432:
             await client.process_commands(message)
-        elif message.content[0] == "!":
+        elif message.content[0] in prefixes:
             await message.author.send(next(status))
+
+@client.command(brief = 'Prefix hozzáadása')
+async def add_prefix(ctx, prefix):
+    global prefixes
+    if prefix in prefixes:
+        await ctx.channel.send('Már létezik, te literális majom! :rofl:')
+    else:
+        prefixes.append(prefix)
+        await ctx.channel.send(f'Added `{prefix}` to the prefix list. :ok_hand:')
+
+@client.command(brief = 'Prefix eltávolítása')
+async def remove_prefix(ctx, prefix):
+    if prefix in prefixes:
+        prefixes.remove(prefix)
+        await ctx.channel.send(f'Removed `{prefix}` from the prefix list.')
+    else:
+        await ctx.channel.send('Nincs ilyen prefix! :angry::anger:')
+
+@client.command(brief = 'Prefixek listája")
+async def prefixlist(ctx):
+    msgs = '['
+    msg = ''
+    for x in prefixes:
+        msg = msg+ msgs+ x+'] '
+    await ctx.channel.send(f'```{msg}```')
+
 
 @client.command(brief='Betölti az összes cogot.')
 async def loadall(ctx):
@@ -64,6 +96,6 @@ async def kys(ctx):
     else:
         await ctx.channel.send("Te nem sorozhatsz be! :stuck_out_tongue_closed_eyes:")
 
-client.run("")
+client.run("NzQxMDAwMDA0MTcyNTEzMzEw.XyxMCA.9V0HurhKc26xayH5Cns4Yjw0HdU")
 
 
