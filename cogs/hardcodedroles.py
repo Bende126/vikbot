74afed5589ad04ddb07cc84fbc28904cac817d1f
Bendegúz Kardos <bendi2001@gmail.com>
2021-10-26 14:54:10 +0200

    mostmár jó roleadós dolog

--- cogs/hardcodedroles.py
+++ cogs/hardcodedroles.py
@@ -1,4 +1,3 @@
-from asyncio.windows_events import NULL
 import discord
 from discord.ext import commands, tasks
 import asyncio
@@ -14,6 +13,27 @@ gm_roles = [
     "894181680875712572" #8. apex
     ]
 
+ev_roles = [
+    "882334562095607868", #2018
+    "881250749521223680", #2019
+    "881250853950996531", #2020
+    "881250917113020488"  #2021
+]
+
+ka_roles=[
+    "739566045743939765", #villany
+    "739565921047150784", #mérnök
+    "739566632803893329"  #üzem
+]
+
+ga_roles = [
+    "744645871861366814", #piros 
+    "744644137499885629", #kék
+    "744645865825632296", #fekete
+    "744644982396289219", #fehér
+    "744644695216619571"  #sárga
+]
+
 class roleosch(commands.Cog):
     def __init__(self, client):
         self.client = client
@@ -26,9 +46,21 @@ class roleosch(commands.Cog):
     async def on_member_update(self, before, after):
         if before.guild.id != 737284142462402560:
             return
+
+        bfroles = before.roles
+        afroles = after.roles
+        
+        bfroles.sort()
+        afroles.sort()
+
+        if bfroles == afroles:
+            return 
         #roles = await after.guild.fetch_roles()
         roles = before.guild.roles
+        print(roles)
         generic_ev = generic_ga = generic_ka = gm_role = None
+        addedroles = []
+        removedroles = []
         #print(roles)
         for x in roles:
             if str(x.id) =="882332491363524628":
@@ -46,34 +78,43 @@ class roleosch(commands.Cog):
 
         if generic_ev in before.roles and generic_ev not in after.roles:
             for x in after.roles:
-                if "Évfolyam: " in x.name:
-                    await after.remove_roles(x)
+                if str(x.id) in ev_roles:
+                    removedroles.append(x)
+                    #await asyncio.sleep(0.5)
 
         if generic_ga in before.roles and generic_ga not in after.roles:
             for x in after.roles:
-                if "Gárda: " in x.name:
-                    await after.remove_roles(x)
+                if str(x.id) in ga_roles:
+                    removedroles.append(x)
+                    #await asyncio.sleep(0.5)
         
         if generic_ka in before.roles and generic_ka not in after.roles:
             for x in after.roles:
-                if "Szak: " in x.name:
-                    await after.remove_roles(x)
-
-        await asyncio.sleep(3)
+                if str(x.id) in ka_roles:
+                    removedroles.append(x)
+                    #await asyncio.sleep(0.5)
 
         for x in after.roles:
-            if "Évfolyam: " in x.name:
-                await after.add_roles(generic_ev)
-                await asyncio.sleep(1)
-            elif "Szak: " in x.name:
-                await after.add_roles(generic_ka)
-                await asyncio.sleep(1)
-            elif "Gárda: " in x.name:
-                await after.add_roles(generic_ga)
-                await asyncio.sleep(1)
+            if str(x.id) in ev_roles:
+                #await after.add_roles(generic_ev)
+                addedroles.append(x)
+                #await asyncio.sleep(0.5)
+            elif str(x.id) in ka_roles:
+                #await after.add_roles(generic_ka)
+                #await asyncio.sleep(0.5)
+                addedroles.append(x)
+            elif str(x.id) in ga_roles:
+                #await after.add_roles(generic_ga)
+                #await asyncio.sleep(0.5)
+                addedroles.append(x)
             elif str(x.id) in gm_roles:
-                await after.add_roles(gm_role)
-                await asyncio.sleep(1)
+                #await after.add_roles(gm_role)
+                #await asyncio.sleep(0.5)
+                addedroles.append(x)
+        await after.remove_roles(*removedroles)
+        print("added roles")
+        await after.add_roles(*addedroles)
+        print("removed roles")
 
     @commands.command()
     async def testcog_roleosch(self, ctx):
